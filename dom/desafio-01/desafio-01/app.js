079a06a0ece6601042a91c21c1c81531422fb891
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-12-11 18:03:39 -0300

    initial commit

---
+++ dom/desafio-01/desafio-01/app.js
@@ -0,0 +1,9 @@
+
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Ronaldo Antonucci',
+        idade: '23',
+        imagem: 'https://i2.wp.com/viladosilicio.com.br/wp-content/uploads/2018/01/vuejs-logo.jpg?fit=740%2C452&ssl=1://www.google.com/imgres?imgurl=https%3A%2F%2Fi2.wp.com%2Fviladosilicio.com.br%2Fwp-content%2Fuploads%2F2018%2F01%2Fvuejs-logo.jpg%3Ffit%3D740%252C452%26ssl%3D1&imgrefurl=https%3A%2F%2Fviladosilicio.com.br%2Fcriando-seu-boilerplate-para-vuejs%2F&docid=_pGpRpHlUv2VzM&tbnid=9PFiVP_xdM4cmM%3A&vet=10ahUKEwiLprHVtK7mAhUIIbkGHUnhC5wQMwhRKAIwAg..i&w=740&h=452&bih=783&biw=492&q=imagens%20vuejs&ved=0ahUKEwiLprHVtK7mAhUIIbkGHUnhC5wQMwhRKAIwAg&iact=mrc&uact=8'
+    }
+})
