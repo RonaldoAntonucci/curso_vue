531e11725125005780d6ce5124dff58a3788cf38
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-12-13 18:45:53 -0300

    vue cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{titulo}}
+    <button @click="titulo += '#'">Botao</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      titulo: 'Teste Data usando VueJs!!'
+    }
+  }
+}
+</script>
+
+<style >
+#app {
+  background-color: chocolate;
+}
+</style>
