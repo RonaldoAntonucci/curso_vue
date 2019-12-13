d5d886bf82ea2e54e4020f102d1d4c7bb0353272
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-12-13 14:26:51 -0300

    desafio finalizado

--- matador-de-monstro/app.js
+++ matador-de-monstro/app.js
@@ -3,13 +3,10 @@ new Vue({
 	data: {
 		pLife: 100,
 		mLife: 100,
-		logs: [{
-			jogador: 'JOGADOR ATINGIU MONSTRO COM 9.',
-			monstro: 'MONSTRO ATINGIU JOGADOR COM 12.'
-		}],
+		logs: [],
 		panel: {
 			width: '100%',
-			backgroundColor: '#ddd',
+			backgroundColor: '#bbb',
 			display: 'flex',
 			flex: 1,
 			justifyContent: 'space-around',
@@ -33,12 +30,12 @@ new Vue({
 			border: 'solid 1px'
 		},
 		actions: {
-			backgroundColor: 'green',
 			width: '100%',
 			height: '150px',
 			display: 'flex',
 			alignItems: 'center',
-			justifyContent: 'center'
+			justifyContent: 'center',
+			backgroundColor: '#ddd'
 		},
 		button: {
 			fontSize: '1.3rem',
@@ -52,11 +49,48 @@ new Vue({
 			flexDirection: 'column',
 			width: '100%',
 			alignItems: 'center',
-			justifyContent: 'flex-start'
+			justifyContent: 'flex-start',
+			color: 'white',
 		},
 		logRow: {
 			display: 'flex',
-			flexDirection: 'column'
+			flexDirection: 'column',
+			margin: '5px 0',
+			width: '100%',
+			textAlign: 'center',
+			fontWeight: 'bold',
+		},
+		logContent: {
+			padding: '10px',
+			borderRadius: '5px'
+		},
+		final: {
+			width: '100%',
+			height: '150px',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			justifyContent: 'center',
+		},
+		blue: {
+			backgroundColor: 'blue',
+			color: 'white'
+		},
+		red: {
+			backgroundColor: 'red',
+			color: 'white',
+		},
+		green: {
+			backgroundColor: 'green',
+			color: 'white'
+		},
+		grey: {
+			backgroundColor: 'grey',
+			color: 'black'
+		},
+		orange: {
+			backgroundColor: 'orange',
+			color: 'black'
 		}
 	},
 	computed: {
@@ -69,28 +103,62 @@ new Vue({
 		},
 		playerLife () {
 			return {
-				backgroundColor: 'red',
+				backgroundColor: this.pLife > 30 ? 'green' : 'red',
 				width: this.pLife + '%'
 			}
 		},
 		monsterLife () {
 			return {
-				backgroundColor: 'red',
+				backgroundColor: this.mLife > 30 ? 'green' : 'red',
 				width: this.mLife + '%'
 			}
-		}
+		},
+		gameOver () {
+			return (this.mLife > 0 && this.pLife > 0) ? false : this.pLife > 0 ? 'Você ganhou!' : 'Você perdeu!';
+		},
 	},
 	methods: {
+		getRandomInt (min, max) {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		},
 		ataque () {
-			const monsterAttack = Math.random(8, 12);
-			const playerAttack = Math.random(6, 9);
-			this.pLife -= monsterAttack;
-			this.mLife -= playerAttack;
-			this.logs.push[{
-				jogador: 'JOGADOR ATINGIU MONSTRO COM 9.',
-				monstro: 'MONSTRO ATINGIU JOGADOR COM 12.'
-			}]
-			console.log(this.logs)
+			const monsterAttack = this.getRandomInt(8, 12);
+			const playerAttack = this.getRandomInt(6, 9);
+			const newPLife = this.pLife - monsterAttack
+			const newMLife = this.mLife - playerAttack
+			this.pLife = newPLife >= 0 ? newPLife : 0;
+			this.mLife = newMLife >= 0 ? newMLife : 0;
+			this.logs.push(`JOGADOR ATINGIU MONSTRO COM ${playerAttack}.`)
+			this.logs.push(`MONSTRO ATINGIU JOGADOR COM ${monsterAttack}.`)
+		},
+		ataEspecial () {
+			const monsterAttack = this.getRandomInt(6, 9);
+			const playerAttack = this.getRandomInt(8, 12);
+			const newPLife = this.pLife - monsterAttack
+			const newMLife = this.mLife - playerAttack
+			this.pLife = newPLife >= 0 ? newPLife : 0;
+			this.mLife = newMLife >= 0 ? newMLife : 0;
+			this.logs.push(`JOGADOR ATINGIU MONSTRO COM ${playerAttack}.`)
+			this.logs.push(`MONSTRO ATINGIU JOGADOR COM ${monsterAttack}.`)
+		},
+		curar () {
+			const monsterAttack = this.getRandomInt(7, 12);
+			const cura = this.getRandomInt(5, 13);
+			const newPLife = this.pLife - monsterAttack + cura
+			this.pLife = newPLife > 100 ? 100 : newPLife < 0 ? 0 : newPLife;
+			this.logs.push(`JOGADOR CUROU ${cura}.`)
+			this.logs.push(`MONSTRO ATINGIU JOGADOR COM ${monsterAttack}.`)
+		},
+		sair () {
+			this.pLife = 0;
+		},
+		newGame () {
+			this.pLife = 100
+			this.mLife = 100
+			this.gameOver = null
+			this.logs = []
 		}
 	}
 });
