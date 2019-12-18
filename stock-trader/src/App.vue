91d1d751349ba65a4ea46497095bd35fe7c9c718
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-12-18 14:31:31 -0300

    projeto stock trader finalizado

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -3,9 +3,14 @@
     <Header></Header>
     <v-content>
       <v-container>
-        <router-view>
+        <transition
+          name="slide"
+          mode="out-in"
+        >
+          <router-view>
 
-        </router-view>
+          </router-view>
+        </transition>
       </v-container>
     </v-content>
   </v-app>
@@ -14,10 +19,42 @@
 <script>
 import Header from './components/Header'
 export default {
-  components: { Header }
+  components: { Header },
+  created () {
+    this.$store.dispatch('initStocks')
+  }
 }
 </script>
 
 <style>
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 0.3s ease;
+}
+
+.slide-leave-active {
+  animation: slide-out 0.3s ease;
+}
 </style>
 
--- stock-trader/src/components/portfolio/Stock.vue
+++ stock-trader/src/components/portfolio/Stock.vue
@@ -1,10 +1,65 @@
 <template>
-  <h1>Ação (Portfólio)</h1>
+  <v-flex
+    class="pr-3 pb-3"
+    xs12
+    md6
+    lg4
+  >
+    <v-card class="blue darken-3 white--text">
+      <v-card-title class="hedaline">
+        <strong>
+          {{stock.name}}
+          <small>(Preço: {{stock.price | currency}} | Qtde: {{stock.quantity}})
+          </small>
+        </strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          :error="insufficientQuantity || !Number.isInteger(quantity)"
+          label="Quantidade"
+          type="number"
+          v-model.number="quantity"
+        ></v-text-field>
+        <v-btn
+          class="blue darken-3 white--text"
+          @click="sellStock"
+          :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+        >{{insufficientQuantity ? 'Insufficient' : 'Vender'}}</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
+  props: ['stock'],
+  data () {
+    return {
+      quantity: 0,
+    }
+  },
+  computed: {
+    insufficientQuantity () {
+      return this.quantity > this.stock.quantity
+    }
+  },
+  methods: {
+    ...mapActions({ sellStockAction: 'sellStock' }),
+    sellStock () {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
 
+      this.sellStockAction(order)
+
+      this.quantity = 0;
+    }
+  }
 }
 </script>
 
