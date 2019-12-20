bd620daa76727bd6241ecf3a936f3b5fc82f55e3
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-12-20 13:38:37 -0300

    definição do grid

--- knowledge-master/versao-inicial/frontend/src/components/template/Header.vue
+++ knowledge-master/versao-inicial/frontend/src/components/template/Header.vue
@@ -12,5 +12,9 @@ export default {
 .header {
   grid-area: header;
   background: linear-gradient(to right, #1e469a, #49a7c1);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
